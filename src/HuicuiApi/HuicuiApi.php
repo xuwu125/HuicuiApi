3ec93b803f04869d9256fa5c89a48e44159d53b0
Changhe <xuwu125@gmail.com>
2017-04-27 14:39:15 +0800

    Api Name

--- src/HuicuiApi/HuicuiApi.php
+++ src/HuicuiApi/HuicuiApi.php
@@ -17,7 +17,7 @@ use GuzzleHttp\Client as GuzzleHttpClient;
  */
 class HuicuiApi
 {
-    const VERSION = '1.1.1';
+    const VERSION = '1.1.2';
     /**
      * 请求的协议，支持 http 和 https
      */
@@ -34,6 +34,9 @@ class HuicuiApi
      * TOKEN 的最长存储时间
      */
     const TOKEN_EXPIRE_TIME = 7100;
+    /**
+     * Client Name
+     */
     const CLIENT_NAME = 'HuicuiApi';
     /**
      * AppID
